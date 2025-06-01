8caeb7b60f6f46ca620fca557187369286efdf06
suyu <13936562@qq.com>
2025-06-01 10:13:40 +0800

    新增巫妖王之怒

---
+++ classic/interface/AddOns/CharacterStatsClassic/Localization/Localization_tw.lua
@@ -0,0 +1,24 @@
+if GetLocale() ~= "zhTW" then return end
+
+-- Translate the strings here
+CSC_DEFENSE                 = DEFENSE;
+CSC_WEAPON_SKILLS_HEADER    = "武器技能";
+CSC_WEAPON_AXE1H_TXT        = "單手斧";
+CSC_WEAPON_AXE2H_TXT        = "雙手斧";
+CSC_WEAPON_MACE1H_TXT       = "單手錘";
+CSC_WEAPON_MACE2H_TXT       = "雙手錘";
+CSC_WEAPON_POLEARM_TXT      = "長柄武器";
+CSC_WEAPON_SWORD1H_TXT      = "單手劍";
+CSC_WEAPON_SWORD2H_TXT      = "雙手劍";
+CSC_WEAPON_STAFF_TXT        = "法杖";
+CSC_WEAPON_UNARMED_TXT      = "拳套";
+CSC_WEAPON_DAGGER_TXT       = "匕首";
+CSC_HIT_BIZNICKS_TXT        = HIT;
+
+CSC_SPELL_HIT_TOOLTIP_TXT       = "Spell Hit chance from gear: %d%%";
+CSC_SPELL_HIT_SUBTOOLTIP_TXT    = "Spell Hit chance (Gear + Talents):";
+CSC_ARCANE_SPELL_HIT_TXT        = "Arcane Spell Hit";
+CSC_FIRE_SPELL_HIT_TXT          = "Fire Spell Hit";
+CSC_FROST_SPELL_HIT_TXT         = "Frost Spell Hit";
+CSC_DESTRUCTION_SPELL_HIT_TXT   = "Destruction Spell Hit";
+CSC_AFFLICTION_SPELL_HIT_TXT    = "Affliction Spell Hit";
---
+++ classic/interface/AddOns/VendorPrice/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Vendor Price
+
+## [1.3.5](https://github.com/ketho-wow/VendorPrice/tree/1.3.5) (2020-04-03)
+[Full Changelog](https://github.com/ketho-wow/VendorPrice/compare/1.3.4...1.3.5)
+
+- Auctionator still does not play nice  
